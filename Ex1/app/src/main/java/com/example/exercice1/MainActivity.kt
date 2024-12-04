a57127618d5c2b38dd6bd8645b7cd68deebf761f
atifiAmine <amine.atifi@lycee-jeanrostand.fr>
2024-12-04 17:05:58 +0100

    début ex1

---
+++ Ex1/app/src/main/java/com/example/exercice1/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.exercice1
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnRouge: Button
+    private lateinit var btnVert: Button
+    private lateinit var btnBleu: Button
+    private lateinit var myLayout: RelativeLayout
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout)
+
+        // Initialisation des boutons et du RelativeLayout
+        btnBleu = findViewById(R.id.buttonBleu)
+        btnVert = findViewById(R.id.buttonVert)
+        btnRouge = findViewById(R.id.buttonRouge)
+        myLayout = findViewById(R.id.myLayout)  // Initialize the RelativeLayout
+
+        // Actions des boutons
+        btnBleu.setOnClickListener {
+            myLayout.setBackgroundColor(Color.BLUE)  // Change l'arrière-plan en bleu
+            val myToast = Toast.makeText(this, "Il fait bleu", Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+            myToast.show()
+        }
+
+        btnVert.setOnClickListener {
+            myLayout.setBackgroundColor(Color.GREEN)  // Change l'arrière-plan en vert
+            val myToast = Toast.makeText(this, "Il fait vert", Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+            myToast.show()
+        }
+
+        btnRouge.setOnClickListener {
+            myLayout.setBackgroundColor(Color.RED)  // Change l'arrière-plan en rouge
+            val myToast = Toast.makeText(this, "Il fait rouge", Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+            myToast.show()
+        }
+    }
+}
