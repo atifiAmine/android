ddef8b5caa124bd68a73b99e60391ab1126c8e26
unknown <amine.atifi@lycee-jeanrostand.fr>
2024-12-08 00:24:08 +0100

    Ex3 fini

--- Ex3/app/src/main/java/com/example/ex3/MainActivity.kt
+++ Ex3/app/src/main/java/com/example/ex3/MainActivity.kt
@@ -1,41 +1,46 @@
 package com.example.ex3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnOn: ImageButton
-    private lateinit var btnOff: ImageButton
+
+    private lateinit var toggleButton: ToggleButton
     private lateinit var myLayout: FrameLayout
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOn = findViewById(R.id.buttonOn)
-        btnOff = findViewById(R.id.buttonOff)
+
+        toggleButton = findViewById(R.id.ToggleButton)
         myLayout = findViewById(R.id.myLayout)
 
-        // Action pour le bouton "On"
-        btnOn.setOnClickListener {
-            myLayout.setBackgroundResource(R.drawable.led_on)  // Change l'arrière-plan en image LED allumée
-            val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
-            myToast.show()
-        }
+        toggleButton.setOnClickListener {
+            if (toggleButton.isChecked) {
+                myLayout.setBackgroundResource(R.drawable.led_on)
+                toggleButton.setBackgroundResource(R.drawable.toggle_off)// Change l'arrière-plan en bleu
+                val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            } else {
+                myLayout.setBackgroundResource(R.drawable.led_off)
+                toggleButton.setBackgroundResource(R.drawable.toggle_on)// Change l'arrière-plan en bleu
+                val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            }
 
-        // Action pour le bouton "Off"
-        btnOff.setOnClickListener {
-            myLayout.setBackgroundResource(R.drawable.led_off)  // Change l'arrière-plan en image LED éteinte
-            val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
-            myToast.show()
+
         }
     }
 }
