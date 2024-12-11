5997dbc31ac147ac461cbb0b3ecd2b2b1d9657a8
atifiAmine <amine.atifi@lycee-jeanrostand.fr>
2024-12-11 11:04:44 +0100

    finalité interface application

--- Raspberry/app/src/main/java/com/example/raspberry/MainActivity.kt
+++ Raspberry/app/src/main/java/com/example/raspberry/MainActivity.kt
@@ -11,13 +11,25 @@ import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.raspberry.R
 import android.graphics.Color
+import android.widget.ImageView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var Chambre: ToggleButton
-    private lateinit var myLayout: FrameLayout
+    private lateinit var Salon: ToggleButton
+    private lateinit var Salledebain: ToggleButton
+    private lateinit var Garage: ToggleButton
+    private lateinit var chambreImage: ImageView
+    private lateinit var salonImage: ImageView
+    private lateinit var salledebainImage: ImageView
+    private lateinit var garageImage: ImageView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,17 +38,84 @@ class MainActivity : AppCompatActivity() {
 
 
         Chambre = findViewById(R.id.chambre)
+        Salon = findViewById(R.id.salon)
+        Salledebain = findViewById(R.id.salledebain)
+        Garage = findViewById(R.id.garage)
+        chambreImage = findViewById(R.id.chambreimage)
+        salonImage = findViewById(R.id.salonimage)
+        salledebainImage = findViewById(R.id.salledebainimage)
+        garageImage = findViewById(R.id.garageimage)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://67a59a64-6711-4258-a10f-363a62e3a180.mock.pstmn.io/") // URL de ton serveur mock
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        apiService = retrofit.create(ApiService::class.java)
 
 
-        Chambre.setOnClickListener {
-            if (Chambre.isChecked) {
-                myLayout.setBackgroundResource(R.drawable.vert)
+        Chambre.setOnCheckedChangeListener { _,isChecked ->
+            var ledstate = if (isChecked) "on" else "off"
+            if (isChecked) {
+                chambreImage.setImageResource(R.drawable.vert)
                 val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
                 myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
                 myToast.show()
             } else {
 
-                Chambre.setBackgroundColor(Color.RED)// Change l'arrière-plan en bleu
+                chambreImage.setImageResource(R.drawable.rouge)// Change l'arrière-plan en bleu
+                val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            }
+            controlLight("Chambre",ledstate)
+
+
+        }
+
+        Salon.setOnClickListener {
+            if (Salon.isChecked) {
+                salonImage.setImageResource(R.drawable.vert)
+                val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            } else {
+
+                salonImage.setImageResource(R.drawable.rouge)// Change l'arrière-plan en bleu
+                val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            }
+
+
+        }
+
+        Salledebain.setOnClickListener {
+            if (Salledebain.isChecked) {
+                salledebainImage.setImageResource(R.drawable.vert)
+                val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            } else {
+
+                salledebainImage.setImageResource(R.drawable.rouge)// Change l'arrière-plan en bleu
+                val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            }
+
+
+        }
+
+            Garage.setOnClickListener {
+            if (Garage.isChecked) {
+                garageImage.setImageResource(R.drawable.vert)
+                val myToast = Toast.makeText(this, "Lumière allumée", Toast.LENGTH_SHORT)
+                myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
+                myToast.show()
+            } else {
+
+                garageImage.setImageResource(R.drawable.rouge)// Change l'arrière-plan en bleu
                 val myToast = Toast.makeText(this, "Lumière éteinte", Toast.LENGTH_SHORT)
                 myToast.setGravity(Gravity.CENTER, 0, 0) // Affiche le toast au centre
                 myToast.show()
